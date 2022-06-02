37c646296450ccf55166fe4ac5c72ae01ec2872a
sdmnny <sdmnny@gmail.com>
2022-06-02 18:45:24 -0400

    created read me sections on index script

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown.js");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+    {
+        type: "input",
+        name:
+        message:
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
